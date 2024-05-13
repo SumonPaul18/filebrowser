ba00a3d4ded17525f86261d068bc723b602fdd51
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-13 09:55:09 +0600

    Create runfilebrowser.sh

---
+++ runfilebrowser.sh
@@ -0,0 +1,8 @@
+docker run \
+    -v /root/filebrowser/data:/srv \
+    -v /root/filebrowser/database/filebrowser.db:/database/filebrowser.db \
+    -v /root/filebrowser/conf/settings.json:/config/settings.json \
+    -e PUID=$(id -u) \
+    -e PGID=$(id -g) \
+    -p 8080:80 \
+    filebrowser/filebrowser:s6
